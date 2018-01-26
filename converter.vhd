e8bb1a577121a6999cd609f1d86c3f894e819d59
pcuji <pcuji@hawk.iit.edu>
2018-01-26 17:21:57 -0600

    Add files via upload

---
+++ converter.vhd
@@ -0,0 +1,4 @@
+notes about FPGA 
+
+A logic cell consists of a lookup table, a flip flop, and connection to adjacent cells. ... A logic slice consists of 2 logic cells. Xilinx counts closer to 2.25 logic cells per slice because they can do more per configurable logic block (CLB) than other architectures.
+
